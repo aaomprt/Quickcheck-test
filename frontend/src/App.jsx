d9de3f90a09db492fa9363ce768d95e51e4c68af
aaomprt <aompanruethai@hotmail.com>
2026-02-13 15:29:55 +0700

    config line liff path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate, useLocation } from "react-router-dom";
 
 import AssessCarDamage from "./page/AssessCarDamage";
 import ResultAssess from "./page/ResultAssess";
@@ -6,14 +6,32 @@ import Register from "./page/Register";
 import Member from "./page/Member";
 import ServiceMap from "./page/ServiceMap";
 
+function LiffRedirectHandler() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(location.search);
+    const liffState = searchParams.get('liff.state');
+
+    if (liffState) {
+      navigate(liffState, { replace: true });
+    }
+  }, [location, navigate]);
+
+  return null;
+}
+
 export default function App() {
   return (
     <>
+      <LiffRedirectHandler />
+
       <Routes>
         <Route path="/assess-car-damage" element={<AssessCarDamage />} />
         <Route path="/assess-car-damage/result/:historyId" element={<ResultAssess />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/member" element={<Member/>} />
+        <Route path="/member" element={<Member />} />
         <Route path="/map-service" element={<ServiceMap />} />
       </Routes>
     </>
